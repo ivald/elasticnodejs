2b3a54b60e6e3d24ef92aa137b499db7d6577ccd
ilya-valdman <ilya.valdman@adesa.com>
2020-02-02 23:47:28 -0500

    es library was replaced + es connection code was changed

--- server/api/elastic/search.controller.js
+++ server/api/elastic/search.controller.js
@@ -1,49 +1,46 @@
 'use strict';
 
-const _ = require('lodash');
-const elasticsearch = require('elasticsearch');
+const { Client } = require('@elastic/elasticsearch')
 
-exports.main = async (req, res) => {
+const client = new Client({
+  node: 'http://localhost:9200'
+});
 
-  const client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace',
-    apiVersion: '7.2', // use the same version of your Elasticsearch instance
-  });
-
+async function ping() {
   client.ping({
     // ping usually has a 3000ms timeout
     requestTimeout: 1000
   }, function (error) {
     if (error) {
-      console.trace('elasticsearch cluster is down!');
-      //res.send('elasticsearch cluster is down!');
+      return false
     } else {
-      console.log('All is well');
-      //res.send('All is well');
+      return true;
     }
   });
+};
 
-  const query = {
-    "query": {
-      "multi_match": {
-        "query": " car"
-      }
-    }
-  };
+exports.main = async (req, res) => {
 
-  const response = await client.search({
+  if(!ping()) return;
+
+  const { body } = await client.search({
     index: 'vehicles_test',
     type: '_doc',
-    body: query
+    body: {
+      query: {
+        multi_match: {
+          query: 'car'
+        }
+      }
+    }
   });
 
-  const vehicles = [];
+    const vehicles = [];
 
-  for (const v of response.hits.hits) {
-    vehicles.push(v);
-  }
+    for (const v of body.hits.hits) {
+      vehicles.push(v);
+    }
 
-  res.send(vehicles);
+    res.send(vehicles);
 
 };
